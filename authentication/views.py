c3e4653da7df2761b529c4d031353d51d23d3f43
Lucas Freire <lucasfreiredev1@gmail.com>
2024-03-21 20:54:04 -0300

    Add Messages to settings and to Cadastro html

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .utils import senha_e_valida
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 # Create your views here.
 
 def cadastro(request):
@@ -11,9 +14,18 @@ def cadastro(request):
         usuario = request.POST.get('usuario')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
+        
         if not senha_e_valida(request, senha):
             return redirect('/auth/cadastro')
-        return HttpResponse('testando')
+        try:
+            user = User.objects.create_user(username=usuario, email=email, password=senha, is_active=False)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuario cadastrado com sucesso!')
+            return redirect('/auth/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/auth/cadastro')
+        
 
 
 def login(request):
